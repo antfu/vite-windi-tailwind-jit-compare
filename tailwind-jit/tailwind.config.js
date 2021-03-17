887761132313af2a6cba450a119111696d6d49af
Michel Edighoffer <edimitchel@gmail.com>
2021-03-18 00:44:16 +0100

    fix(tailwindjit) : add color to tailwind config

--- tailwind-jit/tailwind.config.js
+++ tailwind-jit/tailwind.config.js
@@ -1,5 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     '../*.vue',
   ],
+  theme: {
+    extend: {
+      colors: {
+        'light-blue': colors.lightBlue,
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
 }
